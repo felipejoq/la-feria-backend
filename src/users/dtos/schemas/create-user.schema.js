0352c9f3c6e3435abb056a3dfd51efa4a6ce51b9
felipejoq <felipejofreq@gmail.com>
2024-04-13 15:12:54 -0400

    Cambios mensaje de error al registrar usuario

--- src/users/dtos/schemas/create-user.schema.js
+++ src/users/dtos/schemas/create-user.schema.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const createUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required()
     .pattern(new RegExp('^[a-zA-ZñÑáéíóúüÁÉÍÓÚÜ ]{3,30}$'))
-    .error(new Error('El nombre es requerido. Mínimo 3 caracteres y máximo 30. No se permiten números ni carácteres especiales')),
+    .error(new Error('El nombre es requerido. Mínimo 3 caracteres y máximo 30 sin números ni carácteres especiales')),
   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['test', 'com', 'net'] } }).required()
     .error(new Error('El email es requerido, no cumple con el formato o no t iene extensiones permitidas: ["test", "com", "net"]')),
   password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_+]{6,12}$'))
