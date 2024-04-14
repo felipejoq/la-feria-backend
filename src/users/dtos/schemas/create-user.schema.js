4b8f1c09d1a8f05f2abe77aadcff2f7992bc3e0b
felipejoq <felipejofreq@gmail.com>
2024-04-13 23:27:16 -0400

    Cambia requisitos para la contraseña del usuario

--- src/users/dtos/schemas/create-user.schema.js
+++ src/users/dtos/schemas/create-user.schema.js
@@ -6,8 +6,8 @@ export const createUserSchema = Joi.object({
     .error(new Error('El nombre es requerido. Mínimo 3 caracteres y máximo 30 sin números ni carácteres especiales')),
   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['test', 'com', 'net'] } }).required()
     .error(new Error('El email es requerido, no cumple con el formato o no t iene extensiones permitidas: ["test", "com", "net"]')),
-  password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_+]{6,12}$'))
-    .error(new Error('El password es requerido y puede tener letras, números y carácteres especiales. Longitud de 6 a 12 carácteres')),
+  password: Joi.string().required().min(6).max(12)
+    .error(new Error('El password es requerido. Longitud de 6 a 12 carácteres')),
   roles: Joi.array().length(1),
   active: Joi.boolean().default(true)
     .error(new Error('Active debe ser booleano: true o false')),
